b4b8c2d6393bc82a8af10d2cac3214a4a8823c4c
Caroline Belmas <caroline.belmas@gmail.com>
2020-11-16 17:02:21 +0100

    Create the Toy Model

---
+++ db/migrate/20201116143328_create_toys.rb
@@ -0,0 +1,14 @@
+class CreateToys < ActiveRecord::Migration[6.0]
+  def change
+    create_table :toys do |t|
+      t.string :name
+      t.text :description
+      t.string :category
+      t.string :photo
+      t.integer :unit_price
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
