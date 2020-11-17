c52ddc1ec1d56e9e943cadacae8eab2bcf1fb653
Melodie Souksone <msouksone@gmail.com>
2020-11-17 11:00:43 +0100

    fix toy model

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -2,7 +2,7 @@ class Toy < ApplicationRecord
   CATEGORY =["Puériculture et jouets 0-2 ans", "Jouets 2-4 ans", "Jouets 5-7 ans", "Jouets 8-12 ans", "Jeux d'éveil", "Jeux d'imagination", "Loisirs créatifs", "Jeux de construction", "Jeux de société", "Jeux éducatifs", "Jeux Plein air", "Musique"]
   belongs_to :user
   has_many :rents
-  validates :name, :description, :unit_price, :category, presence :true
+  validates :name, :description, :unit_price, :category, presence: true
   validates :unit_price, numericality: true
   validates_inclusion_of :category, in: CATEGORY
   has_one_attached :photo
