e0f63c44ce1455c5619375a5bb49bf1077b827c5
Caroline Belmas <caroline.belmas@gmail.com>
2020-11-20 13:23:55 +0100

    reviews optimised

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :toys
   has_many :rents, through: :toys
   has_many :rents
+  has_many :reviews, through: :toys
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
